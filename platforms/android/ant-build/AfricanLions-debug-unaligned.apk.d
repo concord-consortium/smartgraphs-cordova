6180ccdb7f5c6f4c928b3d3704098de2ec79770e
sfentress <sfentress@concord.org>
2014-06-17 16:10:11 -0400

    Add built Android project

---
+++ platforms/android/ant-build/AfricanLions-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+/Users/sfentress/projects/smartgraphs-cordova/platforms/android/ant-build/AfricanLions-debug-unaligned.apk : \
+/Users/sfentress/projects/smartgraphs-cordova/platforms/android/ant-build/AfricanLions.ap_ \
+/Users/sfentress/projects/smartgraphs-cordova/platforms/android/ant-build/classes.dex \
+/Users/sfentress/projects/smartgraphs-cordova/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/sfentress/projects/smartgraphs-cordova/platforms/android/libs/libGoogleAnalyticsV2.jar \
--- platforms/android/ant-build/SmartGraphssamsfork-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/sfentress/projects/smartgraphs-cordova/platforms/android/ant-build/SmartGraphssamsfork-debug-unaligned.apk : \
-/Users/sfentress/projects/smartgraphs-cordova/platforms/android/ant-build/SmartGraphssamsfork.ap_ \
-/Users/sfentress/projects/smartgraphs-cordova/platforms/android/ant-build/classes.dex \
-/Users/sfentress/projects/smartgraphs-cordova/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/sfentress/projects/smartgraphs-cordova/platforms/android/libs/libGoogleAnalyticsV2.jar \
