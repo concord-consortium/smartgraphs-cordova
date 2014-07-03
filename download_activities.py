f93f856a7c9af36b8a518a06d8371ead4259f305
Noah Paessel <knowuh@gmail.com>
2014-07-03 18:30:43 -0400

    Handle cases where activity URL, activity name, and internal name vary.

--- download_activities.py
+++ download_activities.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import codecs
 import os
 import re
-import urllib
+import urllib2
+import json
+import pdb
 
 """
 usage ./download_activities.python
@@ -13,7 +17,7 @@ Changes:
   * Will copy activity json specified by the source.html file into above path.
 """
 
-js_activity_tempalte = """
+js_activity_template = u"""
 Smartgraphs.activityDocs = Smartgraphs.activityDocs || {};
 Smartgraphs.activityDocs["%s"] =  %s;
 """
@@ -34,24 +38,34 @@ if os.path.exists(menu_in_file):
     regex = re.compile(url_regex, re.IGNORECASE | re.MULTILINE)
     menu_source_html = ""
 
-    with open(menu_in_file,'r') as source:
+    with codecs.open(menu_in_file,'r','utf-8') as source:
         menu_source_html = source.read()
     urls = regex.findall(menu_source_html)
     hashUrls = {}
     for url in urls:
-        act_name = url.split("/")[-2]
+        # this block is trying to standardize the activity name
+        # the URLS don't have comma's but some of the activities
+        # have internal URLS with commas in them, randomly.
+        # Also the URLs sometimes have numbers, but internally not.
+        json_url     = "%s.json" %(url.strip())
+        json_source  = urllib2.urlopen(json_url)
+        json_data    = unicode(json_source.read(),'utf-8')
+        activity_data= json.loads(json_data)
+        ori_act_name = re.sub("\.df[0-9]+","",activity_data[u"_id"])
+        act_name     = re.sub(",","",ori_act_name)
+        json_data    = re.sub(ori_act_name,act_name,json_data)
+        
         act_name = "/shared/%s" % (act_name)
-        json_url = "%s.json" %(url.strip()  )
+        
         filename = "%s.js" %(json_url.split("/")[-2])
         hashUrls[url] = "../index.html#%s" % (act_name)
         filename = os.path.join(sgActivityRoot,filename)
         print("downloading %s" % (json_url))
         print("to %s" % (filename))
-        json_source = urllib.urlopen(json_url)
-        json_data = json_source.read()
 
-        with open(filename,"w") as dest:
-            dest.write(js_activity_tempalte % (act_name,json_data))
+
+        with codecs.open(filename, "w",'utf-8') as dest:
+            dest.write(js_activity_template % (act_name, json_data))
         menu_source_html = re.sub(url,hashUrls[url],menu_source_html)
-    with open(menu_out_file,'w') as output:
+    with codecs.open(menu_out_file,'w','utf-8') as output:
         output.write(menu_source_html)
