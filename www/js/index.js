72772db9fde06091feda6d3f7b1d8ab4a4f0a764
Noah Paessel <knowuh@gmail.com>
2014-07-17 12:39:43 -0400

    Add hidekeyboard and showkeyboard listeners explicitly, instead of listening for textarea focus and keydown events.

--- www/js/index.js
+++ www/js/index.js
@@ -33,11 +33,28 @@ var app = {
         // Here we create a new object which will clobber the Google Analytics
         // array, _gaq, with our plugin wrapper, so that all calls to _gaq.push
         // will use the plugin code instead.
-        window._gaq = new GAPluginWrapper("UA-6899787-41")
+        window._gaq = new GAPluginWrapper("UA-6899787-41");
         app.replaceLinks();
-        app.addTextAreaFocusScroll();
         app.addPageRelayoutListener();
+        document.addEventListener('hidekeyboard', app.onKeyboardHide, false);
+        document.addEventListener('showkeyboard', app.onKeyboardShow, false);
     },
+
+    onKeyboardShow: function() {
+        app.scrollTextIntoView();
+        return true;
+    },
+
+    onKeyboardHide: function() {
+        return true;
+    },
+
+    scrollTextIntoView: function() {
+        setTimeout(function() {
+            $("textarea").each(function(i,el) { el.scrollIntoView(); });
+        }, 500);
+    },
+
 
     replaceLinks: function() {
         MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
@@ -60,20 +77,27 @@ var app = {
         });
     },
 
-    addTextAreaFocusScroll: function() {
+
+
+    // This is an annoying hack to force a relayout of the page when we switch pages.
+    // This is to fix a bug where, on a small screen, being at the bottom of the scroll
+    // on one page and then switching to the next page will layout the new page with
+    // some of the top of the left pane cut off.
+    addPageRelayoutListener: function() {
+        // only add this for smaller devices
+        if (screen.height > 600 && screen.width > 1000) {
+            return;
+        }
+
         MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
         // fired when a mutation occurs
         var observer = new MutationObserver(function() {
-            // if we're on an android device
-            if ($(".android").length > 0) {
-                $("textarea").unbind();
-                $("textarea").bind('focus keydown', function(){
-                    var el = this;
-                    setTimeout(function() {
-                        el.scrollIntoView();
-                    }, 500);
-                });
-            }
+            var el = $(".sc-view.sc-static-layout").first(),
+                position = $(".sc-view.sc-static-layout").first().css("position");
+            // force relayout
+            el.css({position: "fixed"});
+            el[0].offsetTop;     // doing this forces the browser to recalculate the layout, so we don't need a timeout
+            el.css({position: position});
         });
 
         observer.observe(document, {
@@ -113,7 +137,7 @@ var app = {
 var GAPluginWrapper = function(trackingId) {
     this.gaPlugin = window.plugins.gaPlugin;
     this.gaPlugin.init(null, null, trackingId, 10);
-}
+};
 
 // The only event SmartGraphs sends is in the form
 // _gaq.push(["_trackEvent", "SmartGraphs Activities", key, value]);
@@ -122,4 +146,4 @@ GAPluginWrapper.prototype.push = function(arr) {
         value = arr[3];
 
     this.gaPlugin.trackEvent(null, null, "Event", key, value);
-}
+};
