a00e4d31b6db1d030860829087415edfabf6ac54
Noah Paessel <knowuh@gmail.com>
2015-01-21 14:16:43 -0500

    Use master branch of GAPlugin.  Use inappbrowser plugin.

--- install_plugins.sh
+++ install_plugins.sh
@@ -2,7 +2,7 @@ cordova plugin remove org.apache.cordova.statusbar
 cordova plugin add org.apache.cordova.statusbar
 
 cordova plugin remove com.adobe.plugins.GAPlugin
-cordova plugin add https://github.com/phonegap-build/GAPlugin.git#GA-3.0
+cordova plugin add https://github.com/phonegap-build/GAPlugin.git
 
-# cordova plugin remove org.apache.cordova.inappbrowser
-# cordova plugin add org.apache.cordova.inappbrowser
+cordova plugin remove org.apache.cordova.inappbrowser
+cordova plugin add org.apache.cordova.inappbrowser
